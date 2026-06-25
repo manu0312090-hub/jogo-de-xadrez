ca92ad63b6c458bcd9b6b8544b98c07f33dd7d92
manu0312090-hub <manu0312090@gmail.com>
2026-06-25 20:45:40 -0300

    Peca.java
    Refactored Peca class to include color and code attributes, and added abstract mover method.

--- Peca.java
+++ Peca.java
@@ -1,48 +1,13 @@
-
 public abstract class Peca {
-    private String nome;
-
-    public Peca(String nome) {
-        this.nome = nome;
+    private String cor;
+    private String codigo; 
+    public Peca(String cor, String codigo) {
+        this.cor = cor;
+        this.codigo = codigo;
     }
 
-    public String getNome() {
-        return this.nome;
-    }
-}
+    public String getCor() { return this.cor; }
+    public String getCodigo() { return this.codigo; }
 
-class Rei extends Peca {
-    public Rei(String nome) {
-        super(nome); 
-    }
-}
-
-class Rainha extends Peca {
-    public Rainha(String nome) {
-        super(nome);
-    }
-}
-
-class Torre extends Peca {
-    public Torre(String nome) {
-        super(nome);
-    }
-}
-
-class Bispo extends Peca {
-    public Bispo(String nome) {
-        super(nome);
-    }
-}
-
-class Cavalo extends Peca {
-    public Cavalo(String nome) {
-        super(nome);
-    }
-}
-
-class Peao extends Peca {
-    public Peao(String nome) {
-        super(nome);
-    }
+    public abstract void mover(); 
 }
